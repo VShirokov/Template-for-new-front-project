15650bc74501f2125845e8e4e415c0cd2d6aeb94
Широков Владимир <shirr14@gmail.com>
2018-11-23 23:58:18 +0300

    basic_state

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,3 @@
-setTimeout(function()
-{
-	var max = 100;
-  var tot, str;
-  $('.mainBlock__text').each(function() {
-  	str = String($(this).html());
-  	tot = str.length;
-    str = (tot <= max)
-    	? str
-      : str.substring(0,(max + 1))+"...";
-    $(this).html(str);
-  });
-},500); // Delayed for example only. 
+$(document).ready(function() {
+  
+});
